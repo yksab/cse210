f3ebad38d5636e00d684585375cdbd783250a09b
yksab <baskykeefe@gmail.com>
2025-09-06 16:12:24 +1200

    finished exercis 1, week 1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,18 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+
+        Console.WriteLine("");
+
+        Console.Write("What is your first name? ");
+        string first_name = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string last_name = Console.ReadLine();
+
+        Console.WriteLine("");
+
+        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
+        Console.WriteLine("");
     }
 }
