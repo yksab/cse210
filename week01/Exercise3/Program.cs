ae7624a67b8f66774cef5257c9280e137f191f1e
yksab <baskykeefe@gmail.com>
2025-09-06 20:45:31 +1200

    finished exercise 3, week 1

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,52 @@
 using System;
+using System.Diagnostics;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        // Console.Write("What is the magic number? ");
+        // string magic = Console.ReadLine();
+        // int magicNumber = int.Parse(magic);
+
+        string playAngain;
+        playAngain = "yes";
+
+        while (playAngain == "yes")
+        {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 100);
+            int guessNuber;
+            guessNuber = -1;
+            int tries;
+            tries = 1;
+            while (magicNumber != guessNuber)
+            {
+                Console.WriteLine("Enter a number.");
+                string guess = Console.ReadLine();
+                int guessNumber = int.Parse(guess);
+
+                tries = tries + 1;
+
+                if (magicNumber > guessNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (magicNumber == guessNumber)
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+                else if (magicNumber < guessNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+            }
+            Console.WriteLine($"You tried {tries} times.");
+            Console.WriteLine("Would you like to play again? ");
+            string playAgain = Console.ReadLine();
+        }
+
     }
 }
