491f59db60ce26729c60eeb2c6dbcf84174edf56
yksab <baskykeefe@gmail.com>
2025-09-07 08:47:33 +1200

    as

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -5,5 +5,27 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise5 Project.");
+
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+
+        static string PromptUserName()
+        {
+            Console.WriteLine("What is your name? ");
+            string name = ReadLine();
+
+            return name;
+        }
+
+        static int PrompUserNumber()
+        {
+            Console.WriteLine("What is your favorite number? ");
+            int number = Console.ReadLine();.
+            01
+        }
+
+
     }
 }
