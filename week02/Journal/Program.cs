c4353cd5d8eb8bdf63821887f315c179bb062354
yksab <baskykeefe@gmail.com>
2025-09-20 17:23:32 +1200

    az

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        Console.WriteLine("Menu\n1. new entry\n2. Display journal\n3. save journal\n4.load jornal");
+
+        string choice = Console.ReadLine();
+
+        if (choice = 1)
+        {
+            //
+        }
+        else (choice = 2)
+        {
+            //
+        }
+        else(choice = 3)
+        {
+            //
+        }
+        else(choice = 4)
+        {
+            //
+        }
+        
     }
 }
